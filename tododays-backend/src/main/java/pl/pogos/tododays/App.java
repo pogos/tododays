bf3e74b431d4196799ea481cae299ea4fdf2c2ef
pogos <pogos@o2.pl>
2016-03-16 21:53:09 +0100

    Create application structure

---
+++ tododays-backend/src/main/java/pl/pogos/tododays/App.java
@@ -0,0 +1,13 @@
+package pl.pogos.tododays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
