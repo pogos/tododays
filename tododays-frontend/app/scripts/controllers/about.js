40c9ab9f4117363b01c0018315d722c699c2688b
pogos <pogos@o2.pl>
2016-03-16 22:55:16 +0100

    Add frontend

---
+++ tododays-frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tododaysFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tododaysFrontendApp
+ */
+angular.module('tododaysFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ tododays-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tododaysFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tododaysFrontendApp
+ */
+angular.module('tododaysFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
